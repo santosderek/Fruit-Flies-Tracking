877333a84a9508b714bb0c0d4f0a82d3c100d17a
Derek Santos <derekhalo@gmail.com>
2017-04-21 19:59:12 -0400

    Performance upgrade.
    Main loop now always gets a normal frame to show. Then if contours are detected, the contours will be shown on top of the normal frame. Also I have made a new function called check_windows that will detect if the 'q' key has been pressed to exit the program.

--- main.py
+++ main.py
@@ -10,6 +10,16 @@ import cv2
 
 STILL_TRACKING = True
 
+def check_windows(frame):
+    global STILL_TRACKING
+    
+    cv2.imshow('Image', frame)
+
+    # If key == 'q' then close all windows and stop main loop
+    if cv2.waitKey(1) == ord('q'):
+        cv2.destroyAllWindows()
+        STILL_TRACKING = False
+
 def main():
 
     global STILL_TRACKING
@@ -19,16 +29,24 @@ def main():
     video = VideoStream(resolution = RESOLUTION, framerate = FRAMERATE).start_detection()
     
     print ('Tracking has started...')
-
+    
     while STILL_TRACKING:
-
+        
         contours = video.get_contours()
+        # Allowed Contours will be drawn on top of the threshold image
+        #contour_drawn_frame = video.get_threshold_image()
+        contour_drawn_frame = video.get_frame()
 
         # If there are no contours, skip the whole process
         if contours == None or len(contours) == 0:
+            check_windows(contour_drawn_frame)
+            
+            if not STILL_TRACKING:
+                video.end_stream()
+                
             continue
         
-        print (len(contours))
+        #print (len(contours))
         motion_found = False
         
         # Allowed contours will be a list of position within the contours list that are considered 'worthy' to track
@@ -40,22 +58,18 @@ def main():
                 motion_found = True
                 allowed_contours.append(pos)
 
-        # Allowed Contours will be drawn on top of the threshold image
-        contour_drawn_frame = video.get_threshold_image()
         for position in allowed_contours:
             # Remember that the frame is in Grayscale so the only colors displayed would be black or gray
             contour_drawn_frame = cv2.drawContours(contour_drawn_frame, contours[position], -1, (128, 255, 0), 3)
 
         if DEBUG == True:
 
-            cv2.imshow('Image', contour_drawn_frame)
-
-            # Clean up before program closes
-            if cv2.waitKey(1) == ord('q'):
-                cv2.destroyAllWindows()
+            check_windows(contour_drawn_frame)
+            
+            if not STILL_TRACKING:
                 video.end_stream()
 
-                STILL_TRACKING = False
+            
 
 if __name__ == '__main__':
     
