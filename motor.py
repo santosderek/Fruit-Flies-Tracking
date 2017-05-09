6a811c42fd36f57a279e8af5a3fd2cfba8f06860
Derek Santos <derekhalo@gmail.com>
2017-05-08 21:42:16 -0400

    Importing Ryan Reeds Single File
    This is a single file for Ryan Reeds implementation of the stepper motor that I will be using for my program.

---
+++ motor.py
@@ -0,0 +1,30 @@
+# This file is created by: Ryan Reed
+# Formatted by: Derek Santos
+
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_StepperMotor
+import atexit
+
+class Motor:
+        
+        def __init__(self): 
+                self.hat = Adafruit_MotorHAT(addr = 0x60)
+                self.stepper = self.hat.getStepper(1000,1)
+                self.stepper.setSpeed(1000)
+
+        def release_motors(self):
+                self.hat.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+                self.hat.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+                self.hat.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+                self.hat.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+                
+        def turnOffMotors(self):
+                atexit.register(self.release_motors)
+
+        def step(self):
+                self.turnOffMotors()
+                self.stepper.step(400, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.DOUBLE)
+                
+if __name__ == '__main__':
+        myMotor = Motor()
+        myMotor.step()
+
